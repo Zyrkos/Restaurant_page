99bd24551196127dca8295af2ab6541cc883b72e
Zyrkos <pedrocastro1995@hotmail.com>
2023-03-31 17:25:34 +0100

    initial commit

---
+++ src/modules/nav.js
@@ -0,0 +1,29 @@
+
+
+
+
+
+
+
+    const container = document.getElementById("container")
+    
+
+
+
+
+
+
+
+
+
+  
+
+
+/* const homeDiv = document.createElement("div");
+homeDiv.classList.add("home-div");
+const headline = document.createElement("h1");
+const headlineText = document.createTextNode("Hello, welcome to plant based Sintra")
+headline.appendChild(headlineText)
+homeDiv.appendChild(headline)
+
+container.appendChild(homeDiv)  */
