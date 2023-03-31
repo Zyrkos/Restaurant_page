03f5330379c5d59d2536afdb333f0543d9c52900
Zyrkos <pedrocastro1995@hotmail.com>
2023-03-31 20:09:28 +0100

    added the function to create 'main' content div

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import { createMenu } from "./modules/menu";
 import { createContact } from "./modules/contact"; */
 
 const website = () => {
+  const container = document.getElementById("container");
   const createHeader = () => {
-    const container = document.getElementById("container");
     const header = document.createElement("div");
     header.classList.add("header");
 
@@ -16,19 +16,19 @@ const website = () => {
 
   const createNavbar = () => {
     const nav = document.createElement("nav"); // fixed variable name
-    nav.className = "navbar";
+    nav.classList.add("navbar");
     nav.id = "navbar";
 
     const linkData = [
-      { id: "link-home", text: "Home" },
-      { id: "link-menu", text: "Menu" },
-      { id: "link-contact", text: "Contact" },
+      { id: "link-home", text: "Home", href: "#" },
+      { id: "link-menu", text: "Menu", href: "#" },
+      { id: "link-contact", text: "Contact", href: "#" },
     ];
 
-    const links = linkData.map(({ id, text }) => {
+    const links = linkData.map(({ id, text, href }) => {
       const link = document.createElement("a");
       link.id = id;
-      link.href = "#";
+      link.href = href;
       link.textContent = text;
       return link; // Return link element
     });
@@ -42,14 +42,24 @@ const website = () => {
     return nav; // Return nav element
   };
 
+  const createMain = () => {
+    const main = document.createElement("main");
+    main.classList.add("main");
+
+    const flexWrapper = document.createElement("div");
+    flexWrapper.classList.add("flex-wrapper");
+
+    const contentDiv = document.createElement("div");
+    contentDiv.classList.add("content-div");
+
+    container.appendChild(main);
+    main.appendChild(flexWrapper);
+    flexWrapper.appendChild(contentDiv);
+  };
+
+  
   createHeader();
+  createMain();
 };
 
 website(); // Call the website function to create the header and navbar
-
-/* const createMain = () => {
-      const main = document.createElement("main");
-      main.className = "main";
-      main.id = "main";
-      return main;
-    }; */
